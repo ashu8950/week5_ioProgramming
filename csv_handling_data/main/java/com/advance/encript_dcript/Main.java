9eff8af603dab5b65299060173fee86a029f6126
Ashu <shabsingh961@gmail.com>
2025-04-04 14:46:19 +0530

    [ASHU] Refractor validating encript data and dcripting and writing in file

--- csv_handling_data/main/java/com/advance/encript_dcript/Main.java
+++ csv_handling_data/main/java/com/advance/encript_dcript/Main.java
@@ -7,14 +7,13 @@ public class Main {
     public static void main(String[] args) throws CsvValidationException {
         String csvFilePath = "C:\\Users\\DELL\\Desktop\\IOProgramming\\data_handling\\src\\main\\java\\com\\advance\\encript_dcript\\employee_data.csv"; 
 
-        // Create instances of writer and reader
         CsvWriterWithEncryption writer = new CsvWriterWithEncryption();
-        CsvReaderWithDecryption reader = new CsvReaderWithDecryption();
-
-        // Write data to CSV with encryption
+        //CsvReaderWithDecryption reader = new CsvReaderWithDecryption();
+        
+         //Write data to CSV with encryption
         writer.writeCsv(csvFilePath);
 
         // Read and decrypt data from the CSV file
-        reader.readCsv(csvFilePath);
+        //reader.readAndDecryptCsv(csvFilePath);
     }
 }
